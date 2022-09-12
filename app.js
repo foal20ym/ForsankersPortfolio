38b820a07bab4e06f2639c5aa31dffa843b3a144
foal20ym <alexander-forsanker2011@hotmail.com>
2022-09-12 13:38:10 +0200

    Created the Projects page.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
+const data = require('./data.js')
 
 const app = express()
+
+app.use(express.static('public'))
 
 app.engine('hbs', expressHandlebars.engine({
     defaultLayout: 'main.hbs',
@@ -13,6 +16,14 @@ app.use(
 
 app.get('/', function(request, response){
     response.render('home.hbs')
+})
+
+app.get('/Projects', function(request, response){
+
+    const model = {
+        projects: data.projects
+    }
+    response.render('projects.hbs', model)
 })
 
 app.listen(8080)
---
+++ data.js
@@ -0,0 +1,13 @@
+exports.projects = [{
+    id: 1, 
+    title: "Equalizer",
+    grade: 9 
+},{
+    id: 2,
+    title: "Shrek",
+    grade: 8
+},{
+    id: 3,
+    title: "Repo Men",
+    grade: 10
+}]
