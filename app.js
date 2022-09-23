d2fe185a880bfba6159c971f245513d7089a7d99
foal20ym <alexander-forsanker2011@hotmail.com>
2022-09-23 13:46:43 +0200

    Added authentication and login req for adding proj

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const expressHandlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const path = require('path')
 const sqlite3 = require('sqlite3')
+const expressSession = require('express-session')
+
+const MAX_TITLE_LENGTH = 40
+const MIN_TITLE_LENGTH = 6
+const MAX_DESCRIPTION_LENTGH = 255
+const MIN_DESCRIPTION_LENTGH = 20
+
+const ADMIN_USERNAME = "Alice"
+const ADMIN_PASSOWRD = "abc123"
 
 
 const db = new sqlite3.Database("portfolio-database.db")
@@ -18,6 +27,8 @@ db.run(`
 
 
 const multer = require("multer")
+const { response } = require('express')
+const { request } = require('http')
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "public")
@@ -51,19 +62,37 @@ app.use(bodyParser.urlencoded({
     extended: false
 }))
 
+app.use(
+    expressSession({
+        saveUninitialized: false,
+        resave: false,
+        secret: "yhvsbodiuv"
+    })
+)
+
 // uses and renders the Home page
 app.get('/', function (request, response) {
-    response.render('home.hbs')
+
+    const model = {
+        session: request.session
+    }
+
+    response.render('home.hbs', model)
+
 })
 
 // uses and renders the Contact page
 app.get('/contact', function (request, response) {
+
     response.render('contact.hbs')
+
 })
 
 // uses and renders the About page
 app.get('/about', function (request, response) {
+
     response.render('about.hbs')
+
 })
 
 // renders the projects page
@@ -73,22 +102,18 @@ app.get('/projects', function (request, response) {
 
     db.all(query, function (error, projects) { // this is asyncronius
 
+        const errorMessages = []
+
         if (error) {
-            console.log(error)
-            const model = {
-                projects,
-                dbError: true
-            }
-            response.render('projects.hbs', model)
+            errorMessages.push("Internal server error")
         }
 
-        else {
-            const model = {
-                projects, 
-                dbError: false
-            }
-            response.render('projects.hbs', model)
+        const model = {
+            errorMessages,
+            projects
         }
+
+        response.render('projects.hbs', model)
 
     })
 
@@ -138,22 +163,75 @@ app.post("/add-project", upload.single("image"), (req, res) => {
     const title = req.body.title
     const description = req.body.description
 
-    const query =
-        `INSERT INTO  projects (title, description, image) VALUES (?, ?, ?)`
+    const errorMessages = []
 
-    const values = [title, description, image]
+    if (title == "") {
+        errorMessages.push("Title can't be empty")
+    }
+    else if (MAX_TITLE_LENGTH < title.length) {
+        errorMessages.push("Title can't be more than " + MAX_TITLE_LENGTH + " characters long.")
+    }
+    else if (title.length < MIN_TITLE_LENGTH) {
+        errorMessages.push("Title can't be less than " + MIN_TITLE_LENGTH + " characters long.")
+    }
 
-    db.run(query, values, function (error) {
+    if (description == "") {
+        errorMessages.push("Description can't be empty")
+    }
+    else if (MAX_DESCRIPTION_LENTGH < description.length) {
+        errorMessages.push("Description can't be more than " + MAX_DESCRIPTION_LENTGH + " characters long.")
+    }
+    else if (description.length < MIN_DESCRIPTION_LENTGH) {
+        errorMessages.push("Description can't be less than " + MIN_DESCRIPTION_LENTGH + " characters long.")
+    }
 
-        if (error) {
-            console.log(error)
+    if (!request.session.isLoggedIn) {
+        errorMessages.push("Not logged in.")
+    }
+
+    if (errorMessages.length == 0) {
+
+        const query =
+            `INSERT INTO  projects (title, description, image) VALUES (?, ?, ?)`
+
+        const values = [title, description, image]
+
+        db.run(query, values, function (error) {
+
+            if (error) {
+
+                errorMessages.push("Internal server error")
+
+                const model = {
+                    errorMessages,
+                    title,
+                    description,
+                    image
+                }
+
+                res.render('add-project.hbs', model)
+            }
+
+            else {
+
+                res.redirect("/projects")
+
+            }
+
+        })
+
+    } else {
+
+        const model = {
+            errorMessages,
+            title,
+            description,
+            image
         }
 
-        else {
-            res.redirect("/projects")
-        }
+        res.render('/add-projects.hbs', model)
 
-    })
+    }
 
 })
 
@@ -167,34 +245,22 @@ app.get("/update-project/:id", function (request, response) {
 
     db.get(query, values, function (error, project) {
 
-        if (error) {
-            console.log(error)
-
-            const model = {
-                project,
-                dbError: true
-            }
-            response.render('update-project.hbs', model)
-
+        const model = {
+            project,
         }
 
-        else {
-            const model = {
-                project,
-                dbError: false
-            }
-            response.render('update-project.hbs', model)
-        }
+        response.render('update-project.hbs', model)
 
     })
 
 })
 
+// FIXA DENNA SAMMA SOM ADD-PROJECT
 // Updates the info && takes the user back to the project page
 app.post("/update-project/:id", function (request, response) {
 
     // UPDATE projects SET (title, description, image) = ("Taxi Service app", "The app was made for the company Lyft. I was very inexperienced as a developer at the time and i did learn a lot of new technologies and skills such as Java and React.","app2unsplash.jpg") WHERE id = 2
-
+    
     const id = request.params.id
     const title = request.body.title
     const description = request.body.description
@@ -206,13 +272,7 @@ app.post("/update-project/:id", function (request, response) {
 
     db.run(query, values, function (error) {
 
-        if (error) {
-            console.log(error)
-        }
-
-        else {
             response.redirect("/projects")
-        }
 
     })
 
@@ -239,6 +299,34 @@ app.post("/delete-project/:id", function (request, response) {
     })
 
 })
+
+app.get("/login", function (reguest, response) {
+    response.render('login.hbs')
+})
+
+app.post("/login", function (request, response) {
+    const username = request.body.username
+    const password = request.body.password
+
+    if (username == ADMIN_USERNAME && password == ADMIN_PASSOWRD) {
+
+        request.session.isLoggedIn = true
+
+        response.redirect('/')
+
+    }
+    else {
+
+        const model = {
+            failedToLogin: true // false?
+        }
+
+        response.render('login.hbs', model)
+
+    }
+
+})
+
 
 app.listen(3000)
 
