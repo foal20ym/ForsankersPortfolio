f2f496ed97db7699ce130ab914fa04fe59f6f14d
foal20ym <alexander-forsanker2011@hotmail.com>
2022-10-10 18:02:04 +0200

    Prettier CSS

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const expressHandlebars = require('express-handlebars')
-const bodyParser = require('body-parser')
-const multer = require('multer')
-const path = require('path')
-const sqlite3 = require('sqlite3')
-const expressSession = require('express-session')
-const SQLiteStore = require('connect-sqlite3')(expressSession)
-const db = new sqlite3.Database('portfolio-database.db')
+const express = require("express");
+const expressHandlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const path = require("path");
+const sqlite3 = require("sqlite3");
+const expressSession = require("express-session");
+const SQLiteStore = require("connect-sqlite3")(expressSession);
+const db = new sqlite3.Database("portfolio-database.db");
 
 db.run(`
     CREATE TABLE IF NOT EXISTS projects (
@@ -15,7 +15,7 @@ db.run(`
         description TEXT,
         image TEXT
     )
-`)
+`);
 
 db.run(`
     CREATE TABLE IF NOT EXISTS questions (
@@ -26,7 +26,7 @@ db.run(`
         answer TEXT,
         date TEXT
     )
-`)
+`);
 
 db.run(`
     CREATE TABLE IF NOT EXISTS comments (
@@ -35,53 +35,49 @@ db.run(`
         projectID INTEGER,
         FOREIGN KEY(projectID) REFERENCES projects (projectID)
     )
-`)
+`);
 
+const app = express();
 
-const app = express()
+app.use(express.static("public"));
 
-app.use(express.static('public'))
+app.engine(
+	"hbs",
+	expressHandlebars.engine({
+		extname: "hbs",
+		defaultLayout: "main",
+		partialsDir: __dirname + "/views/partials",
+	})
+);
 
-app.engine('hbs', expressHandlebars.engine({
-    extname: 'hbs',
-    defaultLayout: 'main',
-    partialsDir: __dirname + '/views/partials'
-
-}))
-
-app.use(express.static('public'))
-
-app.use(bodyParser.urlencoded({
-
-    extended: false
-
-}))
+app.use(express.static("public"));
 
 app.use(
+	bodyParser.urlencoded({
+		extended: false,
+	})
+);
 
-    expressSession({
-        saveUninitialized: false,
-        resave: false,
-        secret: 'yhvsbodiuv',
-        store: new SQLiteStore()
-    })
-
-)
+app.use(
+	expressSession({
+		saveUninitialized: false,
+		resave: false,
+		secret: "yhvsbodiuv",
+		store: new SQLiteStore(),
+	})
+);
 
 app.use(function (request, response, next) {
+	const isLoggedIn = request.session.isLoggedIn;
 
-    const isLoggedIn = request.session.isLoggedIn
+	response.locals.isLoggedIn = isLoggedIn;
 
-    response.locals.isLoggedIn = isLoggedIn
+	next();
+});
 
-    next()
+app.use(require("./routers"));
 
-})
-
-app.use(require('./routers'))
-
-
-app.listen(8080)
+app.listen(8080);
 /*
 [nodemon] 2.0.19
 [nodemon] to restart at any time, enter `rs`
@@ -89,23 +85,23 @@ app.listen(8080)
 [nodemon] watching extensions: js,mjs,json
 [nodemon] starting `node app.js`
 node:events:491
-      throw er; // Unhandled 'error' event
-      ^
+	  throw er; // Unhandled 'error' event
+	  ^
 
 Error: listen EADDRINUSE: address already in use :::3000
-    at Server.setupListenHandle [as _listen2] (node:net:1432:16)
-    at listenInCluster (node:net:1480:12)
-    at Server.listen (node:net:1568:7)
-    at Function.listen (/Users/alexanderforsanker/ptest/project_test/node_modules/express/lib/application.js:635:24)
-    at Object.<anonymous> (/Users/alexanderforsanker/ptest/project_test/app.js:105:5)
-    at Module._compile (node:internal/modules/cjs/loader:1126:14)
-    at Object.Module._extensions..js (node:internal/modules/cjs/loader:1180:10)
-    at Module.load (node:internal/modules/cjs/loader:1004:32)
-    at Function.Module._load (node:internal/modules/cjs/loader:839:12)
-    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:81:12)
+	at Server.setupListenHandle [as _listen2] (node:net:1432:16)
+	at listenInCluster (node:net:1480:12)
+	at Server.listen (node:net:1568:7)
+	at Function.listen (/Users/alexanderforsanker/ptest/project_test/node_modules/express/lib/application.js:635:24)
+	at Object.<anonymous> (/Users/alexanderforsanker/ptest/project_test/app.js:105:5)
+	at Module._compile (node:internal/modules/cjs/loader:1126:14)
+	at Object.Module._extensions..js (node:internal/modules/cjs/loader:1180:10)
+	at Module.load (node:internal/modules/cjs/loader:1004:32)
+	at Function.Module._load (node:internal/modules/cjs/loader:839:12)
+	at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:81:12)
 Emitted 'error' event on Server instance at:
-    at emitErrorNT (node:net:1459:8)
-    at processTicksAndRejections (node:internal/process/task_queues:83:21) {
+	at emitErrorNT (node:net:1459:8)
+	at processTicksAndRejections (node:internal/process/task_queues:83:21) {
   code: 'EADDRINUSE',
   errno: -48,
   syscall: 'listen',
@@ -114,4 +110,3 @@ Emitted 'error' event on Server instance at:
 }
 [nodemon] app crashed - waiting for file changes before starting...
 */
-
